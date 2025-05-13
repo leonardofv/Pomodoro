6693b784c3d2a918dbaa2216fc13fc5a10bde39b
leonardo_fonteles <fonteles.leonardo@gmail.com>
2025-05-13 11:58:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/button";
+
+
+export function App() {
+
+  return (
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  )
+}
